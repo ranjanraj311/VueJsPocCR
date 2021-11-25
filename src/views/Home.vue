75beb3fe2ce6211c536e56faa5ad7f1e18010f86
Ranjan Kumar <Ranjan.Kumar@coforge.com>
2021-11-25 16:34:22 +0530

    Added "Go to Customer Page" link button on the Home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
 <nav class="level">
   <div class="level-item has-text-centered">
-    <div>
-      <p class="heading">Registered Customers</p>
-      <p class="title">{{data_customer}} Record Found</p>
-    </div>
+      <div>
+          <p class="heading">Registered Customers</p>
+          <p class="title">{{data_customer}} Record Found</p>
+          <p></p>
+          <p></p>
+          <p class="heading">
+              <router-link :to="`/customer`">
+                  Go To Customer Page
+              </router-link>
+          </p>
+      </div>
   </div>
 </nav>
 </template>
